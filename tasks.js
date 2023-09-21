47790de24420658ba8951e72276ef8e9eff704dd
povilius <povilius@yahoo.com>
2023-09-21 22:07:01 +0300

    8, 9, 10 tasks

--- tasks.js
+++ tasks.js
@@ -89,3 +89,78 @@ function monthsToMinutes (num) {
 }
 
 monthsToMinutes(2)
+
+// 8. Funkciją, kuri konvertuoja dienas į pasirinktą formatą (minutes, valandas, savaites, mėnesius arba metus):
+// 8.1. Funkcija priima du parametrą: 
+//   8.1.1. Dienų skaičių.
+//   8.1.2. Formatą. Galimos formato reikšmės yra:
+//     8.1.2.1. minutes
+//     8.1.2.2. hours
+//     8.1.2.3. weeks
+//     8.1.2.4. months
+//     8.1.2.5. years
+// 8.2. Funkcija konvertuoja dienas į pasirinktą formatą ir grąžina atsakymą tokiu formatu: 5 days - 7200 minutes.
+
+function convertDays(days, format) {
+  
+  switch (format) {
+    case `minutes`:
+      return `${days} days - ${days * 24 * 60} minutes`
+    case `hours`:
+      return `${days} days - ${days * 24} hours`
+    case `weeks`:
+      return `${days} days - ${days / 7} weeks`
+    case `months`:
+      return `${days} days - ${days / 30} months`
+    case `years`:
+      return `${days} days - ${days / 365} years`
+    default:
+      return `Invalid format`
+  }
+}
+
+console.log(convertDays(2, `hours`))
+
+// 9. Funkciją, kuri patikrina ar skaičius dalinasi iš kito skaičiaus:
+// 9.1. Funkcija priima du parametrą: 
+//   9.1.1. Skaičių, kuris bus dalinamas (dalinys).
+//   9.1.2. Skaičių, iš kurio bus dalinama (daliklis).
+// 9.2. Funkcija grąžina atsakymą tokiu formatu:
+//   9.2.1. Jeigu dalinasi: 10 dalinasi iš 5.
+//   9.2.2. Skaičius 11 nesidalina iš 5. Liekana yra 1.
+
+function numberDivision(enterNum, divitionNum) {
+
+  let remainder = enterNum % divitionNum
+
+  if (enterNum % divitionNum === 0) {
+    return `${enterNum} dalinasi iš ${divitionNum}.`
+  } else {
+    return `Skaičius ${enterNum} nesidalina iš ${divitionNum}. Liekana yra ${remainder}.`
+  }
+}
+
+console.log(numberDivision(2, 3))
+
+// 10. Funkciją, kuri patikrina ar įvestas tekstas turi porini raidžių skaičių ar neporinį.
+
+function evenCount(text) {
+
+  let letterCount = text.length
+
+  if (letterCount % 2 === 0) {
+    return `Žodis ${text} turi porinį skaičių raidžių`
+  } else {
+    return `Žodis ${text} turi neporinį skaičių raidžių`
+  }
+}
+
+console.log(evenCount(`laba`))
+
+// 11. Funkciją, kuri paima nurodytą simbolį iš žodžio ar sakinio. 
+// 11.1. Funkcija priima du parametrą: tekstą ir kelintą simbolį reikia grąžinti.
+// 11.2. Funkcija grąžina atsakymą tokiu formatu: Teksto "Labas" 3 raidė yra "b".
+
+// 11.3. Jeigu nurodytas skaičius yra didesnis nei tekstas turi simbolių, tai reikia grąžinti error'ą tokiu formatu: Tekstas "Labas" turi 5 simbolius, o jūs nurodėte grąžinti 8.
+
+// 11.4. Patobulinti funkciją, kad būtų galima įrašyti neigiamą skaičių, jeigu norima gauti teksto simbolį skaičiuojant nuo jo galo, o ne nuo priekio.
